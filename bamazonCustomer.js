1d20b47a9a4f71a180d92138ac09a3c64efb19c7
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-24 23:24:34 -0700

    Required mysql and console.table packages from npm. Created connection to mysql database bamazon_db and assigned to variable connection. Declared a function which queries all from the products tables and logs the result to the terminal in a readable table format using console.table

---
+++ bamazonCustomer.js
@@ -0,0 +1,31 @@
+var mysql = require("mysql");
+require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_db"
+});
+//login credentials to mysql database
+
+connection.connect((err)=>{
+    if (err) {
+        console.log(err);
+    }
+    inventory();
+    //callback function that displays current inventory stored in database
+});
+
+
+function inventory() {
+    connection.query("SELECT * FROM products", (err, res) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.table(res);
+            //npm package neatly displays response object from database as a readable table
+        };
+    });
+};
