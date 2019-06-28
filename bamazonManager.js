f0bd16b03c49891d7bf10d00e7898e1313c4468d
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-27 22:42:15 -0700

    Declared a function restock that prompts the manager for the requisite id and quantity they would like to restock. The answer.quantity and answer.id are then added as arguments to addInventory as a call back function. addInventory queries all table data from products where the item_id matches the argument id. Then the products table is updated by setting the stock_quantity to the provided amount argument at the argument id. inventory is called to display the table with the restocked data.

--- bamazonManager.js
+++ bamazonManager.js
@@ -16,7 +16,6 @@ connection.connect((err)=>{
         console.log(err);
     }
     runProgram();
-    //callback function that displays current inventory stored in database
 });
 
 function runProgram() {
@@ -39,6 +38,7 @@ function runProgram() {
                 lowInventory();
                 break;
             case "Add to inventory":
+                restock();
                 break;
             case "Add New product":
                 break;
@@ -71,5 +71,38 @@ function lowInventory() {
             console.table(res);
         };
         runProgram();
+    });
+};
+
+function restock() {
+    inquirer.prompt([
+        {
+            name: "ID",
+            type: "input",
+            message: "Which item_id would you like to restock?"
+        },
+        {
+            name: "Quantity",
+            type: "input",
+            message: "How many would you like to add to stock_quantity?"
+        }
+    ]).then(function(answer) {
+        var quantity = answer.Quantity;
+        var itemID = answer.ID;
+        addInventory(itemID, quantity);
+    });
+};
+
+function addInventory(id, amount) {
+    connection.query("SELECT * FROM products WHERE item_id = " + id, function(err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+            connection.query("UPDATE products SET stock_quantity = stock_quantity + ? WHERE item_id = ?", [amount, id]);
+            console.log(chalk.greenBright("-----------------------------------------"));
+            console.log(chalk.greenBright("You added " + amount + " units to item_id " + id));
+            console.log(chalk.greenBright("-----------------------------------------"));
+            inventory();
+        }
     });
 };
